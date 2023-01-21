0a3697f9f9c52cde3490bcae165dc3c584075071
Priyansh Nahar <priyanshnahar1998@gmail.com>
2023-01-21 16:19:42 +0530

    Weather app

---
+++ weather-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Temprature from "./components/temprature";
+
+function App() {
+  return (
+    <>
+      <Temprature />
+    </>
+  );
+}
+
+export default App;
---
+++ weather-app/src/components/temprature.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./css/style.css";
+
+function Temprature() {
+  return (
+    <div className="box">
+      <div className="inputData">
+        <input type="search" className="inputField" defaultValue="" />
+      </div>
+    </div>
+  );
+}
+
+export default Temprature;
